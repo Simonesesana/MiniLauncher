fe8af538f1a2688180848f19a6c6867746df9bfc
Simonesesana <simone.sesana@mail.polimi.it>
2025-10-12 12:12:10 +0200

    Created new flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-/// Pages import
-import 'Pages/Home/Home.dart';
-import 'Pages/Loading/Loading.dart';
-import 'Pages/Settings/Settings.dart';
-import 'package:minilauncher/Pages/Home/Drawer/HomeDrawer.dart';
-import 'package:minilauncher/Preferences/PreferencesClass.dart';
-import 'package:minilauncher/Pages/WelcomePage/WelcomePage.dart';
-import 'Pages/Settings/SettingsSections/AppSelection/SelectFavouriteApps.dart';
-import 'package:minilauncher/Pages/Settings/SettingsSections/AppSelection/SelectRestrictedApps.dart';
-
-/// Preferences object
-PreferencesClass preferences = PreferencesClass();
-
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,19 +29,13 @@ class MyApp extends StatelessWidget {
       title: 'MiniLauncher',
 
       /// Initial route
-      initialRoute: '/loading',
+      initialRoute: '/home',
 
-      /// List of al the app pages
-      routes: {
-        '/home': (context) => const Home(),
-        '/loading': (context) => const Loading(),
-        '/settings': (context) => const Settings(),
-        '/home_drawer': (context) => const HomeDrawer(),
-        '/welcome_page': (context) => const WelcomePage(),
-        '/select_favourite_apps': (context) => const SelectFavouriteApps(),
-        '/select_restricted_apps': (context) => const SelectRestrictedApps(),
-      },
+        routes: {
+          //'/home': (context) => const Home(),
+        }
 
-    );
+
+        );
   }
 }
